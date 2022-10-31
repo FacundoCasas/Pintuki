92b58b38a410ac5d7ff464adc0659c7bede976ff
JuanFerreyros <juansferreyros@gmail.com>
2022-10-31 20:45:11 -0300

    Categoria Capas (Back)

---
+++ server/src/routes/CategoriaRoute.ts
@@ -0,0 +1,26 @@
+import CategoriaService from "../service/CategoriaService";
+import express from 'express'
+
+export const CategoriaRouter = express.Router();
+
+const categoriaService : CategoriaService = new CategoriaService();
+
+CategoriaRouter.get('', async (req, res) => {
+    console.log("categorias",await categoriaService.findAll())
+    res.json( await categoriaService.findAll())
+})
+  
+CategoriaRouter.post('', async (req, res) => {   
+    await categoriaService.add(req);
+    res.json( {"resultado": "ok"} )
+})
+
+CategoriaRouter.delete('/:id', async (req, res) => {
+    await categoriaService.delete(Number(req.params.id));
+    res.json( {"resultado": "ok"} )    
+})
+
+CategoriaRouter.get('/:id', async (req, res) => {  
+    console.log(req.params.id)  
+    res.json( await categoriaService.get(Number(req.params.id)) )    
+})
