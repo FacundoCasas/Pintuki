4731b57104f3ebcd88b48c7d58eff2f2fec64491
FacundoCasas <75185801+FacundoCasas@users.noreply.github.com>
2022-11-09 19:18:35 -0300

    Cambios Variables de entorno

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,9 +3,10 @@ import { PublicacionRouter } from './routes/PublicacionRoutes';
 import { UsuarioRouter } from './routes/UsuarioRoutes';
 import { CategoriaRouter } from './routes/CategoriaRoutes';
 import cors from 'cors';
-
+import * as dotenv from 'dotenv'
+dotenv.config({path:'variables.env'})
+//process.env.DB_URL
 const app = express();
-const port = 3000;
 app.use(cors())
 app.use(express.json())
 
@@ -17,6 +18,10 @@ app.use("/publicaciones", PublicacionRouter)
 app.use("/usuarios", UsuarioRouter)
 app.use("/categorias", CategoriaRouter)
 
-app.listen(port, () => {
+//leer localhost de variables y puerto
+const host : string = process.env.HOST || '0.0.0.0';
+const port : number  = +process.env.PORT || 3000;
+
+app.listen(port,host, () => {
     return console.log(`Server is listening on ${port}`);
 });
